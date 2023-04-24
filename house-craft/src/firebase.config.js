b16eff3b8ea28c4166b25dd8fb4108c3ec54c95c
ReicaD <dbamanya447@gmail.com>
2023-04-24 02:36:04 -0400

    Added Firebase And React app

---
+++ house-craft/src/firebase.config.js
@@ -0,0 +1,19 @@
+import { getFirestore } from "firebase/app";
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC0eNOxoLyp1Ef6YBKqdgzLGJCX90y06nw",
+  authDomain: "housecraft-app.firebaseapp.com",
+  projectId: "housecraft-app",
+  storageBucket: "housecraft-app.appspot.com",
+  messagingSenderId: "533324193465",
+  appId: "1:533324193465:web:3113b09422d5e961970568",
+};
+
+// Initialize Firebase
+//  const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore();
