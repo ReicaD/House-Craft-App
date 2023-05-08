f7ce97c05fb2627a176feb641c4b2c158ea47158
ReicaD <dbamanya447@gmail.com>
2023-05-08 16:14:09 -0400

    modified with forms in profile

--- house-craft/src/Pages/Profile.jsx
+++ house-craft/src/Pages/Profile.jsx
@@ -1,20 +1,33 @@
 import { useState } from "react";
-import { getAuth } from "firebase/auth";
+import { getAuth, updateProfile } from "firebase/auth";
+import { db } from "../firebase.config";
+import { updateDoc } from "firebase/firestore";
 import { useNavigate, Link } from "react-router-dom";
 
 function Profile() {
   const auth = getAuth();
+  const [changeDetails, setChangeDetails] = useState(false);
   const [formData, setFormData] = useState({
     name: auth.currentUser.displayName,
     email: auth.currentUser.email,
   });
-  const{name,email}= formData
+  const { name, email } = formData;
   const navigate = useNavigate();
   //this will logout form firebase
   const onLogout = () => {
     auth.signOut();
-    navigate("/")
+    navigate("/");
   };
+   const onSubmit= ()=>{
+     console.log(123);
+   }
+
+   const onChange=(e)=>{
+    setChangeDetails((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+   }
   return (
     <div className="profile">
       <header className="profileHeader">
@@ -23,9 +36,33 @@ function Profile() {
           logOut
         </button>
       </header>
+      <main>
+        <div className="perofileDetailsHeader">
+          <p className="profileDetailsText">personal Details</p>
+          <p
+            className="changePersonalDetails"
+            onClick={() => {
+              changeDetails && onSubmit();
+              setChangeDetails((prevState) => !prevState);
+            }}
+          >
+            {changeDetails ? "done" : "change"}
+          </p>
+        </div>
+        {/* //the class below will give dynamic changes depending if the data renders true of false */}
+        <div className="profileCard">
+          <form action="" className="">
+            {/* changing details and disabling the details if the change is false  */}
+            <input type="text" id="name" className={!changeDetails ?"profileName" : "ProfileNameActive" }
+            disabled={!changeDetails}
+            value={name}
+            onChange={onChange}
+            />
+          </form>
+        </div>
+      </main>
     </div>
   );
-  
 }
 
 export default Profile;
